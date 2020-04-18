e756020a244b0f52a256c1ce5130790f2519a3e7
Corendos <corentin.godeau37@gmail.com>
2020-04-18 19:52:54 +0200

    added .env file for secret variables

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
+import sys
+import os
+
 from flask import Flask
 from flask import request, json, render_template
 from predictions import prediction
 from predictions.prediction import generate_statistics_for_request
 from predictions.get_ap_code import get_ap_codes
 from predictions.fuel_consumption import other_transport
+from dotenv import load_dotenv
+load_dotenv()
 
+if os.getenv("GMAPS_API_KEY") is None:
+    sys.exit(-1)
 
 app = Flask(__name__)
 
@@ -14,7 +21,7 @@ app = prediction.init_app(app)
 
 @app.route("/")
 def index_handler():
-    return render_template("index.html")
+    return render_template("index.html", api_key=os.getenv("GMAPS_API_KEY"))
 
 
 @app.route("/statistics", methods=["POST"])
