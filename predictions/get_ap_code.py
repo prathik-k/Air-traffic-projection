f4dec558f6c35cdc24caaa65c3d4916ccc54a7fc
Corendos <corentin.godeau37@gmail.com>
2020-04-19 10:48:19 +0200

    fixed incoherent indentation

--- predictions/get_ap_code.py
+++ predictions/get_ap_code.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 def get_ap_codes(all_airports, lat_lng_or, lat_lng_dest):
-	"""
-	This function accepts the latitude and longitude of the origin destination pairs, and returns all airports in close proximity to the coordinates. 
-	E.g. using (40.713,-74.006) for either coordinate returns "LGA","EWR","ISP" and "JFK" - the airports serving NYC.
-	:param all_airports: List of all airports in mainland US along with coordinates
-	:param lat_lng_or: latitude & longitude of origin as tuple
-	:param lat_lng_dest: latitude & longitude of destination as tuple
-	:return city_pairs: list of tuples of (origin,destination)
-	"""
+        """
+        This function accepts the latitude and longitude of the origin destination pairs, and returns all airports in close proximity to the coordinates.
+        E.g. using (40.713,-74.006) for either coordinate returns "LGA","EWR","ISP" and "JFK" - the airports serving NYC.
+        :param all_airports: List of all airports in mainland US along with coordinates
+        :param lat_lng_or: latitude & longitude of origin as tuple
+        :param lat_lng_dest: latitude & longitude of destination as tuple
+        :return city_pairs: list of tuples of (origin,destination)
+        """
         lat_or,lng_or = lat_lng_or
         lat_dest,lng_dest = lat_lng_dest
 
