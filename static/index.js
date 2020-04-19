fb9820b436204d1f55fe0831d82bc3529ec28ae8
Corendos <corentin.godeau37@gmail.com>
2020-04-19 10:51:17 +0200

    Added some margin to the left of the no data message

--- static/index.js
+++ static/index.js
@@ -583,7 +583,7 @@ searchButton.onclick = function() {
 		    drawGraphs(statisticsData);
 		} else {
 		    noDataDiv = d3.select("body").append("div");
-		    noDataDiv.append("p").text("No direct flights between the cities specified.");
+		    noDataDiv.append("p").attr("id", "no_data").text("No direct flights between the cities specified.");
 		}
 		drawOtherTransportTable(data);
 	    })
